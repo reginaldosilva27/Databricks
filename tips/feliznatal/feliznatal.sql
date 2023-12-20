ee5b98e030a1d5bd6de18be040c98afdf6e22e4f
Reginaldo Silva <69867503+reginaldosilva27@users.noreply.github.com>
2023-12-20 18:01:15 -0300

    Add files via upload

---
+++ tips/feliznatal/feliznatal.sql
@@ -0,0 +1,29 @@
+-- Letra F
+SELECT CAST ('POLYGON((1 10, 1 11, 2 11, 2 10.8, 1.25 10.8, 1.25 10.6, 1.75 10.6, 1.75 10.4, 1.25 10.4, 1.25 10, 1 10))' as geometry) 
+UNION ALL 
+-- Letra E
+SELECT CAST ('POLYGON((2 10, 2 11, 3 11, 3 10.8, 2.25 10.8, 2.25 10.6, 2.75 10.6, 2.75 10.4, 2.25 10.4, 2.25 10.2, 3 10.2, 3 10, 2 10))' as geometry)
+UNION ALL 
+-- Letra L
+SELECT CAST ('POLYGON((3.15 11, 3.15 10, 3.85 10, 3.85 10.2, 3.35 10.2, 3.35 11, 3.15 11))' as geometry)
+UNION ALL 
+-- Letra I
+SELECT CAST ('POLYGON((4.2 11, 4.8 11, 4.8 10.8, 4.6 10.8, 4.6 10.2, 4.8 10.2, 4.8 10, 4.2 10, 4.2 10.2, 4.4 10.2, 4.4 10.8, 4.2 10.8, 4.2 11))' as geometry)
+UNION ALL 
+-- Letra Z
+SELECT CAST ('POLYGON((5 11, 6 11, 5.4 10.2, 6 10.2, 6 10, 5 10, 5.6 10.8, 5 10.8, 5 11))' as geometry)
+UNION ALL 
+-- Letra N
+SELECT CAST ('POLYGON((1 10, 1 9, 1.2 9, 1.2 9.8, 1.8 9, 2 9, 2 10, 1.8 10, 1.8 9.3, 1.3 10, 1 10))' as geometry)
+UNION ALL 
+-- Letra A
+SELECT CAST ('POLYGON((2 9, 2 10, 3 10, 3 9, 2.75 9, 2.75 9.3, 2.25 9.3, 2.25 9, 2 9),(2.25 9.5, 2.25 9.8, 2.75 9.8, 2.75 9.5, 2.25 9.5))' as geometry)
+UNION ALL 
+-- Letra T
+SELECT CAST ('POLYGON((3 9.8, 3 10,4 10, 4 9.8, 3.6 9.8, 3.6 9, 3.4 9, 3.4 9.8, 3 9.8))' as geometry)
+UNION ALL 
+-- Letra A
+SELECT CAST ('POLYGON((4 9, 4 10, 5 10, 5 9, 4.75 9, 4.75 9.3, 4.25 9.3, 4.25 9, 4 9),(4.25 9.5, 4.25 9.8, 4.75 9.8, 4.75 9.5, 4.25 9.5))' as geometry)
+UNION ALL 
+-- Letra L
+SELECT CAST ('POLYGON((5.15 10, 5.15 9, 5.85 9, 5.85 9.2, 5.35 9.2, 5.35 10, 5.15 10))' as geometry)
