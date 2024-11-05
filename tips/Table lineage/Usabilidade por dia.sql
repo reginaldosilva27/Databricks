64c42547a9c8aad19b1eff181c2dd80ddb11f890
Reginaldo Silva <69867503+reginaldosilva27@users.noreply.github.com>
2024-11-04 22:00:01 -0300

    Create Usabilidade por dia.sql

---
+++ tips/Table lineage/Usabilidade por dia.sql
@@ -0,0 +1,68 @@
+-- tabelas mais lidas por dia
+select
+  event_date,
+  catalogName,
+  schemaName,
+  tableName,
+  sum(READS) as READS,
+  sum(WRITES) as WRITES
+from
+  (
+    select
+      event_date,
+      t.table_catalog as catalogName,
+      t.table_schema as schemaName,
+      t.table_name as tableName,
+      sum(
+        case
+          when read.source_table_name is not null then 1
+          else 0
+        end
+      ) READS,
+      0 WRITES
+    from
+      system.information_schema.tables t
+      left join system.access.table_lineage read on t.table_name = read.source_table_name
+      and t.table_schema = read.source_table_schema
+      and t.table_catalog = read.source_table_catalog
+    where
+      t.table_catalog not in('system')
+      and t.table_schema not in('information_schema')
+      and (
+        read.target_type in('TABLE')
+        or read.target_type is null
+      )
+    group by
+      all
+    union all
+    select
+      event_date,
+      t.table_catalog as catalogName,
+      t.table_schema as schemaName,
+      t.table_name as tableName,
+      0 READS,
+      sum(
+        case
+          when write.target_table_name is not null then 1
+          else 0
+        end
+      ) WRITES
+    from
+      system.information_schema.tables t
+      left join system.access.table_lineage write on t.table_name = write.target_table_name
+      and t.table_schema = write.target_table_schema
+      and t.table_catalog = write.target_table_catalog
+    where
+      t.table_catalog not in('system')
+      and t.table_schema not in('information_schema')
+      and (
+        write.target_type in('TABLE')
+        or write.target_type is null
+      )
+    group by
+      all
+  ) Tabs
+group by
+  all
+order by
+  1 desc
